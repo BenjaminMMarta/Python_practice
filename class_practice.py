669f5989041f314f5bde6f91b52651a32395039b
Benjamin Marta <benjaminmarta@icloud.com>
2025-02-03 20:26:15 -0800

    Update class_practice.py

--- class_practice.py
+++ class_practice.py
@@ -1,6 +1,6 @@
-#Creating a Class called "names"
+#Creating a class called "names".
 class names:
-  #Creating contructor with an argument "__init__"
+  #Creating contructor with an argument "__init__".
   def __init__(self, name):
     if name == "Ben" or name == "Madalyn":
       print("Hi, my name is {}!".format(name))
@@ -9,3 +9,10 @@ class names:
 name1 = names("Ben")
 name2 = names("Madalyn")
 name3 = names("Mindy")
+
+#Creating a class called "days".
+class days:
+  #Creating contructor with an argument "__init__".
+  def __init__(self, day)
+  self.day = day
+  
